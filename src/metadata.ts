80a2d0b76903f8bdb3013c9c2f95f19e3527592b
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2025-08-15 10:58:37 +0800

    Add local `.env` file.

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,11 +1,14 @@
 import path = require('path');
 import fs = require('fs');
-import assert = require('assert');
+import dotenv = require('dotenv');
 
 const projectDirectory = path.dirname(__dirname);
 const packageData = JSON.parse(
   fs.readFileSync(path.join(projectDirectory, 'package.json')).toString()
 );
+dotenv.config({
+  path: path.join(projectDirectory, '.env.local'),
+});
 
 export = {
   projectDirectory,
